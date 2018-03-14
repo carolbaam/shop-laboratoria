54037c679f9e75838331be73c4989785aa38de87
Behtsa <behtsabet14@gmail.com>
2018-03-14 10:33:42 -0600

    navBar

--- js/app.js
+++ js/app.js
@@ -77,15 +77,6 @@ const apiLoad = () => {
         });
 };
 
-// const showNavPrice = totalPrice => {
-//   let priceTemplate += ` `;
-//   priceTemplate = `
-//   <p>${counter} Artículos</p>
-//   <h3>Total: ${totalPrice}</h3>
-//   <button>IR AL CARRITO</button>
-//   ` 
-// }
-
 let totalPrice = 0;
 const showInSideBar = productsArray => {
   let carTemplate = ` `;
@@ -104,6 +95,16 @@ const showInSideBar = productsArray => {
     })
   // showNavPrice(totalPrice);
 }
+// const showNavPrice = totalPrice => {
+//   let priceTemplate += ` `;
+//   priceTemplate = `
+//   <p>${counter} Artículos</p>
+//   <h3>Total: ${totalPrice}</h3>
+//   <button>IR AL CARRITO</button>
+//   ` 
+// }
+
+
 
 let productsArray = [];
 const addToCar = (id, title, price) => {
